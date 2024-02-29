e949377ff008f5986f1c0231d6df79f60f882c3b
JinPots <JinPots@users.noreply.github.com>
2024-02-29 11:53:52 +0000

    deploy: 117a0732714b328b0ed7dffc6936a2aa51b8c707

---
+++ _app/immutable/nodes/4.2gx_rjtK.js
@@ -0,0 +1,12 @@
+import{s as o,e as i,c as h,g as c,a as l,i as g,n as e,d as m}from"../chunks/scheduler.D5FNPCS4.js";import{S as d,i as u}from"../chunks/index.CdvyzOZ9.js";function x(r){let n,a=`<div><div class="xl:w-2/3 m-auto left-0 right-0"><div class="card mt-2 mb-2 p-2 bg-yellow text-2xl">🌈 <strong>Vietcombank</strong>
+			1016449287
+			Hoang Thien Trang <br/>
+			🌈 <strong>MBBank</strong>
+			58080402
+			Hoang Thien Trang <br/>
+			🌈 <strong>Momo</strong>
+			0978549194
+			Hoang Thien Trang <br/>
+			🌈 <strong>Card</strong>
+			Giá card sẽ nhỉnh hơn nhiều nên các cậu hãy suy nghĩ kĩ nha, <br/> giá card có thể sẽ update theo ngày. <br/>  
+			🌈 Bồ nào muốn chuyển khoản ở Circle K qua momo của Chanh thì có gì ib trước cho Chanh 1 ngày để Chanh gửi QR Code nha. <br/>❗️ <strong>Hãy chụp lại bill đã chuyển khoản để Chanh confirm nha.</strong> <br/></div></div></div>`;return{c(){n=i("main"),n.innerHTML=a,this.h()},l(t){n=h(t,"MAIN",{class:!0,"data-svelte-h":!0}),c(n)!=="svelte-1c87dw3"&&(n.innerHTML=a),this.h()},h(){l(n,"class","text-center p-2 xl:flex xl:justify-center xl:align-middle xl:h-screen xl:items-center")},m(t,s){g(t,n,s)},p:e,i:e,o:e,d(t){t&&m(n)}}}class f extends d{constructor(n){super(),u(this,n,null,x,o,{})}}export{f as component};
